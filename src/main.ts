0faed062b3b64ecbc19d148ef035e4697fc32874
ehsan <ehsan@raychat.io>
2024-06-03 15:15:58 +0330

    configur main file and load config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './application/application.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig, PORT } from './share/config/app.config';
 // load config
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function loadConfig(): Promise<ConfigService> {
+  const appContext = await NestFactory.createApplicationContext(
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+  );
+  return appContext.get<ConfigService>(ConfigService);
 }
+
+async function bootstrap() {
+  const configService = await loadConfig();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(configService.get(PORT));
+}
+
 bootstrap();
