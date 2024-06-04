9739b5563e510d6e492e13f293308a9c3d8ab392
ehsan <ehsan@raychat.io>
2024-06-04 15:16:23 +0330

    hotfix syntax user model

--- src/infra/database/repository/user.repository.ts
+++ src/infra/database/repository/user.repository.ts
@@ -1,29 +1,29 @@
 import { Injectable, } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { UserDocument } from '../models/user.model';
+import { User, UserDocument } from '../models/user.model';
 
 
 
 @Injectable()
 export class userMongoRepository {
   constructor(
-    @InjectModel('user')
-    private readonly usertModel: Model<UserDocument>,
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
   ) { }
 
   find(query) {
     try {
-      return this.usertModel.find(query);
+      return this.userModel.find(query);
     } catch (err) {
       return err;
     }
   }
 
 
-  findOne(query) {
+  findOne(query, options = {}) {
     try {
-      return this.usertModel.findOne(query);
+      return this.userModel.findOne(query, options);
     } catch (err) {
       return err;
     }
@@ -31,7 +31,7 @@ export class userMongoRepository {
 
   create(input) {
     try {
-      return this.usertModel.create(input);
+      return this.userModel.create(input);
     } catch (err) {
       return err;
     }
@@ -40,7 +40,7 @@ export class userMongoRepository {
 
   delete(id) {
     try {
-      return this.usertModel.deleteOne({ id });
+      return this.userModel.deleteOne({ id });
     } catch (err) {
       return err;
     }
@@ -49,7 +49,7 @@ export class userMongoRepository {
 
   update(id, input) {
     try {
-      return this.usertModel.updateOne({ id }, { $set: input });
+      return this.userModel.updateOne({ id }, { $set: input });
     } catch (err) {
       return err;
     }
