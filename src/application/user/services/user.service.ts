416a54361c4ae1a75b3f6bb7b5ec77b17dd242a9
ehsan <ehsan@raychat.io>
2024-06-04 15:15:52 +0330

    usage of query bus for testing on service

--- src/application/user/services/user.service.ts
+++ src/application/user/services/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { FindOneUserQueryHandler } from '../queries/handlers/find.one.user.handler';
 
 @Injectable()
-export class UserService { }
+export class UserService {
+  constructor(
+    private queryBus: QueryBus,
+    private commandBus: CommandBus,
+  ) { }
+
+  async findOneUser(id: string): Promise<any> {
+    return await this.queryBus.execute(new FindOneUserQueryHandler(id));
+  }
+
+}
